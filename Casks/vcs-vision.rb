1af7099496c041e484b355b2ec745c653aee29a5
Alex Brooke <akb.sbc@gmail.com>
2018-11-26 14:42:21 -0500

    update vision utility URL

--- Casks/vcs-vision.rb
+++ Casks/vcs-vision.rb
@@ -2,7 +2,7 @@ cask 'vcs-vision' do
   version '0.2.3'
   sha256 'a9ca497165f01f39c46d8660b5f20e0a05823df8ceb986230582a7ca179cfeb7'
   
-  url "https://github.com/purduesigbots/pros-cli3/releases/3.1.2/vcs_vision-#{version}-macOS.zip"
+  url "https://github.com/purduesigbots/pros-cli3/releases/download/3.1.2/vcs_vision-#{version}-macOS.zip"
   name 'VCS Vision'
   homepage 'https://www.vexrobotics.com/vexedr/products/programming'
   
