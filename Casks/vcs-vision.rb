8e4a31a513912c51ed2a3bdbac399679a8f63aac
Alex Brooke <akb.sbc@gmail.com>
2019-02-02 12:19:01 -0600

    fix sha checkusm for 0.2.4

--- Casks/vcs-vision.rb
+++ Casks/vcs-vision.rb
@@ -1,6 +1,6 @@
 cask 'vcs-vision' do
   version '0.2.4'
-  sha256 'a9ca497165f01f39c46d8660b5f20e0a05823df8ceb986230582a7ca179cfeb7'
+  sha256 '17aa3e68ef65ad00616d6a0fe13616446ee8dbd1b687b89a1f61020ee275fdd7'
   
   url "https://github.com/purduesigbots/pros-cli3/releases/download/3.1.3/vcs_vision-#{version}-macOS.zip"
   name 'VCS Vision'
