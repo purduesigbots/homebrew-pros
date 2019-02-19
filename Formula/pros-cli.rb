bfea68af7c2050d95847b272597926c654cd07aa
Alex Brooke <akb.sbc@gmail.com>
2019-02-19 14:37:18 -0600

    remove test stanza
    it was broken. may want to revisit that in the future, but for now manual verification is ok

--- Formula/pros-cli.rb
+++ Formula/pros-cli.rb
@@ -119,18 +119,4 @@ class ProsCli < Formula
   def install
     virtualenv_install_with_resources
   end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test pros-cli3`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/prosv5", "c", "n", "."
-    system "#{bin}/prosv5", "c", "info-project"
-  end
 end
