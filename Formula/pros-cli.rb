be77d20c2e07cb6cb67af3b7e3ad50ec8667c8ec
Ayush Shukla <71904196+ayushuk@users.noreply.github.com>
2024-05-18 15:38:37 -0700

    Update pros-cli.rb

--- Formula/pros-cli.rb
+++ Formula/pros-cli.rb
@@ -136,6 +136,11 @@ class ProsCli < Formula
     sha256 "35547502bf1958044716a03a2f47092a89efe8f9789ab0c4c528d9c9c30bc148"
   end
 
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
   resource "markdown-it" do
     url "https://files.pythonhosted.org/packages/38/71/3b932df36c1a044d397a1f92d1cf91ee0a503d91e470cbd670aa66b07ed0/markdown-it-py-3.0.0.tar.gz"
     sha256 "e3f60a94fa066dc52ec76661e37c851cb232d92f9886b15cb560aaada2df8feb"
